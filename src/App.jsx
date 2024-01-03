bf834e782589c1bf021affc0e271edb6f7bd6d43
Luis Cortes <71467212+eligio12@users.noreply.github.com>
2024-01-03 14:39:38 -0600

    Cambio a la api, quitamos lo de node

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   const [alert, setAlert] = useState(false)
   
 
-  const baseUrl = 'https://crud-app-dev-fnfd.3.us-1.fl0.io'
+  const baseUrl = 'https://users-crud.academlo.tech/swagger/?format=openapi'
 
   const [users, getUsers, createUser, deleteUsers, updateUsers, statusAlert, setStatusAlert ] = useFetch(baseUrl)
 
